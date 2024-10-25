2162429af70e2147eb5f2774fc374d8ed53afd33
MysticNacromancer <148905118+mysticNacromancer@users.noreply.github.com>
2024-10-25 18:11:26 +0530

    LargestOfThreeNumbers.java

---
+++ LargestOfThreeNumbers.java
@@ -0,0 +1,19 @@
+//Q) find the largest number from amoung three numbers input from user 
+
+public class LargestNumber {
+  public static void main(String[] args ){
+    Scanner in = new Scanner(System.in);
+    int a = in.nextInt();
+    int b = in.nextInt();
+    int c = in.nextInt();
+
+    int max = a ;
+    if (b > a ) {
+      max = b;
+    }
+    if (c > b) {
+      max = c;
+    }
+    System.out.println("Largest number = " + max );
+  }
+}
