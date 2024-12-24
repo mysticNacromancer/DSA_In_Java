d43f325194de9eb3b82ec28f416dc83bb3a0156d
MysticNacromancer <148905118+mysticNacromancer@users.noreply.github.com>
2024-12-25 00:44:50 +0530

    Create ReverseEnteredNumber.java
    Reversing an integer entered by the user

---
+++ ReverseEnteredNumber.java
@@ -0,0 +1,19 @@
+// Reverse the entered number by the user 
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+  public static void main(String[] args){
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter The Number To Reverse : ");
+    int num = in.nextInt();
+    int res = 0;
+    while(num > 0){
+      int rem = num % 10;
+      res = res * 10 + rem;
+      num = num / 10;
+    }
+     System.out.println("Reverse of the entered number is : " + res);
+  }
+}
